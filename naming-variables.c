5474856a417f1a16fb6f791a862974f53c3b59f9
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-04-21 21:38:41 +0000

    naming variables

---
+++ naming-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    // I deleted the !
+int test_value;
+// I deleted the space and added a _
+int example1;
+// I deleted the 1 and put it last
+float int_1;
+// I added things to the name
+printf("Variables created successfully.\n");
+}
