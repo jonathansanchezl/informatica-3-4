2532cc0e1e5bc19d5251a0ba092f7260ee58fe24
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-04-24 14:48:42 +0000

    hello, color

--- hello.c
+++ hello.c
@@ -2,5 +2,15 @@
 
 int main(void)
 {
-printf("hello Jonathan! \n");
+char name[50];
+char color[50];
+//ask name
+    printf("enter a user: \n");
+    scanf("%s", &name);
+    printf("Hello, %s! \n",name);
+// ask color
+    printf("what is your favorite color?: \n");
+    scanf("%s", &color);
+    printf("Your favorite color is %s! \n",color);
+
 }
