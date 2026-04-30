7110d2851bb36c4f6d0f27a0314f46b05f373a93
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-04-30 15:35:43 +0000

    minutes

--- minutes.c
+++ minutes.c
@@ -5,17 +5,20 @@ int main(void)
     int minutes;
     int duracionM;
     int duracionH;
+
 printf("Movie running time (in minutes):");
 scanf("%d", &minutes);
+
     printf("Start time:");
     scanf("%d:%d", &duracionH, &duracionM);
 
 
     int duracionPH = (minutes/60);
     int duracionPM = (minutes%60);
+
 int FduracionH = duracionH + duracionPH;
 int FduracionM = duracionM + duracionPM;
 
-    printf("The movie will last: %d hours and %d minutes \n",duracionPH,duracionPM);
-printf("End time: %d:%d \n",FduracionH,FduracionM);
+    printf("The movie will last: %d hours and %02d minutes \n",duracionPH,duracionPM);
+printf("End time: %d:%02d \n",FduracionH,FduracionM);
 }
