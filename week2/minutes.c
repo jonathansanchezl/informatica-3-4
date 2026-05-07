4297c7f36b8a66fc87826a277d4a549de8e29f3f
jonathansanchezl <jonathansanchezlozano7@gmail.com>
2026-05-07 19:27:27 +0000

    minutes final

--- week2/minutes.c
+++ week2/minutes.c
@@ -19,6 +19,15 @@ scanf("%d", &minutes);
 int FduracionH = duracionH + duracionPH;
 int FduracionM = duracionM + duracionPM;
 
-    printf("The movie will last: %d hours and %02d minutes \n",duracionPH,duracionPM);
+printf("The movie will last: %d hours and %02d minutes \n",duracionPH,duracionPM);
+if(FduracionM >= 60) {
+ int finalM = FduracionM - 60;
+ int finalH = FduracionH + 1;
+ printf("End time: %d:%02d \n",finalH,finalM);
+}
+else {
+
 printf("End time: %d:%02d \n",FduracionH,FduracionM);
 }
+}
+
